9ae8b0d07092f991501052a7436f5fcaa836cb0d
chris <193523771+chris-biagini@users.noreply.github.com>
2026-08-15 12:41:29 -0400

    feat: cockpit app - workspace deck, volume knob, status OLED

--- firmware/code.py
+++ firmware/code.py
@@ -2,22 +2,9 @@ import supervisor
 
 from adafruit_macropad import MacroPad
 
-from pad.framework import App, run
-
-
-class Blink(App):
-    name = "blink"
-
-    def on_show(self):
-        self.screen.idle_card()
-
-    def on_key_event(self, n, pressed, now):
-        self.pad.pixels[n] = 0x203040 if pressed else 0
-
-    def on_msg(self, msg):
-        self.screen.footer.text = msg.get("t", "?")
-
+from apps.cockpit import Cockpit
+from pad.framework import run
 
 macropad = MacroPad()
 supervisor.runtime.autoreload = True
-run(macropad, [Blink()])
+run(macropad, [Cockpit()])
